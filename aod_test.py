88ef436220ac2ae8e28a79f0bfebee1d1fc96c6c
Pramila-m <88033769+Pramila-m@users.noreply.github.com>
2023-03-30 01:49:41 +0530

    Update aod_test.py

--- aod_test.py
+++ aod_test.py
@@ -116,21 +116,21 @@ if st.button("Predict"):
     #conc=AOD500nm
     c=(math.floor(10*AOD500nm))/10
     if (c>=0 and c<12.1):
-	AQI=Linear(50,0,12,0,c)
+    	AQI=Linear(50,0,12,0,c)
     elif (c>=12.1 and c<35.5):
-	AQI=Linear(100,51,35.4,12.1,c);
+    	AQI=Linear(100,51,35.4,12.1,c);
     elif(c>=35.5 and c<55.5):
-	AQI=Linear(150,101,55.4,35.5,c);
+    	AQI=Linear(150,101,55.4,35.5,c);
     elif(c>=55.5 and c<150.5):
-	AQI=Linear(200,151,150.4,55.5,c);
+    	AQI=Linear(200,151,150.4,55.5,c);
     elif (c>=150.5 and c<250.5):
-	AQI=Linear(300,201,250.4,150.5,c);
+    	AQI=Linear(300,201,250.4,150.5,c);
     elif (c>=250.5 and c<350.5):
-	AQI=Linear(400,301,350.4,250.5,c);
+    	AQI=Linear(400,301,350.4,250.5,c);
     elif(c>=350.5 and c<500.5):
-	AQI=Linear(500,401,500.4,350.5,c);
+    	AQI=Linear(500,401,500.4,350.5,c);
     else:
-	AQI="PM25message";
+    	AQI="PM25message";
     st.success(("Air Quality Index (AQI) Value {} ".format(AQI)))
     phi = user_lat
     longitude = user_lon
