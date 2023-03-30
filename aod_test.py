d06b2fa3380c9c4bcc53203447d16e22077cbbe9
Pramila-m <88033769+Pramila-m@users.noreply.github.com>
2023-03-31 01:14:03 +0530

    Update aod_test.py

--- aod_test.py
+++ aod_test.py
@@ -259,7 +259,7 @@ def home():
 	    ### Extraterrestrial Radiation
 
 	    def etr(n):
-		return G_sc*(1.00011+0.034221*cos(2*pi*(n-1)/365)+0.00128*sin(2*pi*(n-1)/365)+0.000719*cos(2*(2*pi*(n-1)/365))+0.000077*sin(2*(2*pi*(n-1)/365)))
+	    	return G_sc*(1.00011+0.034221*cos(2*pi*(n-1)/365)+0.00128*sin(2*pi*(n-1)/365)+0.000719*cos(2*(2*pi*(n-1)/365))+0.000077*sin(2*(2*pi*(n-1)/365)))
 
 	    ds['ETR'] = [etr(n) for n in ds['DOY']] # append dataframe with etr for day
 
@@ -268,111 +268,111 @@ def home():
 	    ds['Dangle'] = [2*pi*(n-1)/365 for n in ds['DOY']]
 
 	    def decl(Dangle):
-		return (0.006918-0.399912*cos(Dangle)+0.070257*sin(Dangle)-0.006758*cos(2*Dangle)+0.000907*sin(2*Dangle)-0.002697*cos(3*Dangle)+0.00148*sin(3*Dangle))*(180/pi)
+	    	return (0.006918-0.399912*cos(Dangle)+0.070257*sin(Dangle)-0.006758*cos(2*Dangle)+0.000907*sin(2*Dangle)-0.002697*cos(3*Dangle)+0.00148*sin(3*Dangle))*(180/pi)
 	    ds['DEC'] = [decl(Dangle) for Dangle in ds['Dangle']]
 
 	    def eqtime(Dangle):
-		return (0.0000075+0.001868*cos(Dangle)-0.032077*sin(Dangle)-0.014615*cos(2*Dangle)-0.040849*sin(2*Dangle))*229.18
+	    	return (0.0000075+0.001868*cos(Dangle)-0.032077*sin(Dangle)-0.014615*cos(2*Dangle)-0.040849*sin(2*Dangle))*229.18
 	    ds['EQT'] = [eqtime(Dangle) for Dangle in ds['Dangle']]
 
 	    def omega(hr, eqt):
-		return 15*(hr-12.5) + longitude - tz*15 + eqt/4
+	    	return 15*(hr-12.5) + longitude - tz*15 + eqt/4
 	    ds['Hour Angle'] = [omega(hr, eqt) for hr, eqt in zip(ds['HR'],ds['EQT'])]
 
 	    def zen(dec, hr_ang):
-		return acos(cos(dec/(180/pi))*cos(phi/(180/pi))*cos(hr_ang/(180/pi))+sin(dec/(180/pi))*sin(phi/(180/pi)))*(180/pi)
+	    	return acos(cos(dec/(180/pi))*cos(phi/(180/pi))*cos(hr_ang/(180/pi))+sin(dec/(180/pi))*sin(phi/(180/pi)))*(180/pi)
 	    ds['Zenith Ang'] = [zen(dec, hr_ang) for dec, hr_ang in zip(ds['DEC'],ds['Hour Angle'])]
 
 	    def airmass(zenang):
-		if zenang < 89:
-		    return 1/(cos(zenang/(180/pi))+0.15/(93.885-zenang)**1.25)
-		else:
-		    return 0
+	    	if zenang < 89:
+	    		return 1/(cos(zenang/(180/pi))+0.15/(93.885-zenang)**1.25)
+	    	else:
+	    		return 0
 	    ds['Air Mass'] = [airmass(zenang) for zenang in ds['Zenith Ang']]
 
 	    ### Intermediate Results
 
 	    def T_rayleigh(airmass):
-		if airmass > 0:
-		    return exp(-0.0903*(P_mb*airmass/1013)**0.84*(1+P_mb*airmass/1013-(P_mb*airmass/1013)**1.01))
-		else:
-		    return 0
+	    	if airmass > 0:
+	    		return exp(-0.0903*(P_mb*airmass/1013)**0.84*(1+P_mb*airmass/1013-(P_mb*airmass/1013)**1.01))
+	    	else:
+	    		return 0
 	    ds['T rayleigh'] = [T_rayleigh(airmass) for airmass in ds['Air Mass']]
 
 	    def T_ozone(airmass):
-		if airmass > 0:
-		    return 1-0.1611*(Ozone_cm*airmass)*(1+139.48*(Ozone_cm*airmass))**-0.3034-0.002715*(Ozone_cm*airmass)/(1+0.044*(Ozone_cm*airmass)+0.0003*(Ozone_cm*airmass)**2)
-		else:
-		    return 0
+	    	if airmass > 0:
+	    		return 1-0.1611*(Ozone_cm*airmass)*(1+139.48*(Ozone_cm*airmass))**-0.3034-0.002715*(Ozone_cm*airmass)/(1+0.044*(Ozone_cm*airmass)+0.0003*(Ozone_cm*airmass)**2)
+	    	else:
+	    		return 0
 	    ds['T ozone'] = [T_ozone(airmass) for airmass in ds['Air Mass']]
 
 	    def T_gasses(airmass):
-		if airmass > 0:
-		    return exp(-0.0127*(airmass*P_mb/1013)**0.26)
-		else:
-		    return 0
+	    	if airmass > 0:
+	    		return exp(-0.0127*(airmass*P_mb/1013)**0.26)
+	    	else:
+	    		return 0
 	    ds['T gases'] = [T_gasses(airmass) for airmass in ds['Air Mass']]
 
 	    def T_water(airmass):
-		if airmass > 0:
-		    return 1-2.4959*airmass*H20_cm/((1+79.034*H20_cm*airmass)**0.6828+6.385*H20_cm*airmass)
-		else:
-		    return 0
+	    	if airmass > 0:
+	    		return 1-2.4959*airmass*H20_cm/((1+79.034*H20_cm*airmass)**0.6828+6.385*H20_cm*airmass)
+	    	else:
+	    		return 0
 	    ds['T water'] = [T_water(airmass) for airmass in ds['Air Mass']]
 
 	    def T_aerosol(airmass):
-		if airmass > 0:
-		    return exp(-(Taua**0.873)*(1+Taua-Taua**0.7088)*airmass**0.9108)
-		else:
-		    return 0
+	    	if airmass > 0:
+	    		return exp(-(Taua**0.873)*(1+Taua-Taua**0.7088)*airmass**0.9108)
+	    	else:
+	    		return 0
 	    ds['T aerosol'] = [T_aerosol(airmass) for airmass in ds['Air Mass']]
 
 	    def taa(airmass, taerosol):
-		if airmass > 0:
-		    return 1-0.1*(1-airmass+airmass**1.06)*(1-taerosol)
-		else:
-		    return 0
+	    	if airmass > 0:
+	    		return 1-0.1*(1-airmass+airmass**1.06)*(1-taerosol)
+	    	else:
+	    		return 0
 	    ds['TAA'] = [taa(airmass, taerosol) for airmass, taerosol in zip(ds['Air Mass'],ds['T aerosol'])]
 
 	    def rs(airmass, taerosol, taa):
-		if airmass > 0:
-		    return 0.0685+(1-Ba)*(1-taerosol/taa)
-		else:
-		    return 0
+	    	if airmass > 0:
+	    		return 0.0685+(1-Ba)*(1-taerosol/taa)
+	    	else:
+	    		return 0
 	    ds['rs'] = [rs(airmass, taerosol, taa) for airmass, taerosol, taa in zip(ds['Air Mass'],ds['T aerosol'],ds['TAA'])]
 
 	    def Id(airmass, etr, taerosol, twater, tgases, tozone, trayleigh):
-		if airmass > 0:
-		    return 0.9662*etr*taerosol*twater*tgases*tozone*trayleigh
-		else:
-		    return 0
+	    	if airmass > 0:
+	    		return 0.9662*etr*taerosol*twater*tgases*tozone*trayleigh
+	    	else:
+	    		return 0
 	    ds['Id'] = [Id(airmass, etr, taerosol, twater, tgases, tozone, trayleigh) for airmass, etr, taerosol, twater, tgases, tozone, trayleigh in zip(ds['Air Mass'],ds['ETR'],ds['T aerosol'],ds['T water'],ds['T gases'],ds['T ozone'],ds['T rayleigh'])]
 
 	    def idnh(zenang, Id):
-		if zenang < 90:
-		    return Id*cos(zenang/(180/pi))
-		else:
-		    return 0
+	    	if zenang < 90:
+	    		return Id*cos(zenang/(180/pi))
+	    	else:
+	    		return 0
 	    ds['IdnH'] = [idnh(zenang, Id) for zenang, Id in zip(ds['Zenith Ang'],ds['Id'])]
 
 	    def ias(airmass, etr, zenang, tozone, tgases, twater, taa, trayleigh, taerosol):
-		if airmass > 0:
-		    return etr*cos(zenang/(180/pi))*0.79*tozone*tgases*twater*taa*(0.5*(1-trayleigh)+Ba*(1-(taerosol/taa)))/(1-airmass+(airmass)**1.02)
-		else:
-		    return 0
+	    	if airmass > 0:
+	    		return etr*cos(zenang/(180/pi))*0.79*tozone*tgases*twater*taa*(0.5*(1-trayleigh)+Ba*(1-(taerosol/taa)))/(1-airmass+(airmass)**1.02)
+	    	else:
+	    		return 0
 	    ds['Ias'] = [ias(airmass, etr, zenang, tozone, tgases, twater, taa, trayleigh, taerosol) for airmass, etr, zenang, tozone, tgases, twater, taa, trayleigh, taerosol in zip(ds['Air Mass'],ds['ETR'],ds['Zenith Ang'],ds['T ozone'],ds['T gases'],ds['T water'],ds['TAA'],ds['T rayleigh'],ds['T aerosol'])]
 
 	    def gh(airmass, idnh, ias, rs):
-		if airmass > 0:
-		    return (idnh+ias)/(1-albedo*rs)
-		else:
-		    return 0
+	    	if airmass > 0:
+	    		return (idnh+ias)/(1-albedo*rs)
+	    	else:
+	    		return 0
 	    ds['GH'] = [gh(airmass, idnh, ias, rs) for airmass, idnh, ias, rs in zip(ds['Air Mass'],ds['IdnH'],ds['Ias'],ds['rs'])]
 
 	    ### Decimal Time
 
 	    def dectime(doy, hr):
-		return doy+(hr-0.5)/24
+	    	return doy+(hr-0.5)/24
 	    ds['Decimal Time'] = [dectime(doy, hr) for doy, hr in zip(ds['DOY'],ds['HR'])]
 
 	    ### Model Results (W/m^2)
